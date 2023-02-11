2014aa4ffd6f76dbfd930584412739eb1afe7d6e
Brilds <brunomurilo@0804@gmail.com>
2023-02-11 15:12:43 -0300

    ajuste pinh + seboso

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,12 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with pong!'),
-    async execute(interaction) {
-         await interaction.reply('Pong!');
-    },
-};
+        .setDescription('Mostra o ping do bot no Discord'),
+    async execute(message) {
+        const m = message.channel.send("Pinging...");
+        m.then((msg) => {
+          const ping = Date.now() - msg.createdTimestamp;
+          msg.edit(`O ping é de ${ping} ms seu seboso.`);
+        });
+      },
+    };
