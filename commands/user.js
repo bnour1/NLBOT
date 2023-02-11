aeafa38477c06b21e994f95ff5d5842b4065b5d0
bnour1 <brunowashingtong@gmail.com>
2023-02-11 11:27:38 -0300

    Arrumando reply's '/server' '/user'

--- commands/user.js
+++ commands/user.js
@@ -5,7 +5,7 @@ data: new SlashCommandBuilder()
     .setName('user')
     .setDescription('Provides information about the user.'),
 async execute(interaction) {
-    await interaction.reply('This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.');
+    await interaction.reply('This command was run by '+ interaction.user.username + ' , who joined on ' + interaction.member.joinedAt+'.');
 },
     
 };
